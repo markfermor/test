02bd714b669967a44f4d0c6d894093e348829f52
Mark Fermor <mark.fermor@holidayextras.com>
2014-12-18 22:15:16 +0000

    added some comments and fixed some spacing

--- data_import.php
+++ data_import.php
@@ -8,15 +8,19 @@
 	// Contents of file - should be better than array i presume and less memory?
 	$handle = fopen("users.csv", "r");
 	if ($handle) {
+	//read file line by line
     while (($line = fgets($handle)) !== false) {
-        // process the line read.
+        // set the column headings
         $table_field = array('email','lastname','firstname','fav_colours','dob');
+        // Will nice get data correctly and encapsulate the strings in quotes
 		$values = str_getcsv($line);
+		// To make the DOB a european date
 		$values[4] = str_replace('/', "-", $values[4]);
 		$time = strtotime($values[4]);
+		// Get the date into the correct format for insert to MySQL
         $values[4] = date('Y-m-d',$time);
 
-		var_dump($values);
+        // Insert the data
         $conn->insert('people',$table_field,$values);
     }
 	} else {
@@ -24,8 +28,5 @@
     	die('error opening file');
 	} 
 	fclose($handle);
-
-
-
 
 ?>
