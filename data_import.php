0c117483ff7129648b8c8e780d09a668b2ac93cc
Mark Fermor <mark.fermor@holidayextras.com>
2014-12-19 00:41:48 +0000

    sorting out spacing.. its tabs would prefer not but oh well

--- data_import.php
+++ data_import.php
@@ -8,25 +8,24 @@
 	// Contents of file - should be better than array i presume and less memory?
 	$handle = fopen("users.csv", "r");
 	if ($handle) {
-	//read file line by line
-    while (($line = fgets($handle)) !== false) {
-        // set the column headings
-        $table_field = array('email','lastname','firstname','fav_colours','dob');
-        // Will nice get data correctly and encapsulate the strings in quotes
-		$values = str_getcsv($line);
-		// To make the DOB a european date
-		$values[4] = str_replace('/', "-", $values[4]);
-		$time = strtotime($values[4]);
-		// Get the date into the correct format for insert to MySQL
-        $values[4] = date('Y-m-d',$time);
+		//read file line by line
+		while (($line = fgets($handle)) !== false) {
+			// set the column headings
+			$table_field = array('email','lastname','firstname','fav_colours','dob');
+			// Will nicely get data correctly and encapsulate the strings in quotes
+			$values = str_getcsv($line);
+			// To make the DOB a european date - should have data input validation if doing this
+			$values[4] = str_replace('/', "-", $values[4]);
+			$time = strtotime($values[4]);
+			// Get the date into the correct format for insert to MySQL
+			$values[4] = date('Y-m-d',$time);
 
-        // Insert the data
-        $conn->insert('people',$table_field,$values);
-    }
+			// Insert the data
+			$conn->insert('people',$table_field,$values);
+		}
 	} else {
-    	// error opening the file.
-    	die('error opening file');
+		// error opening the file.
+		die('error opening file');
 	} 
 	fclose($handle);
-
 ?>
